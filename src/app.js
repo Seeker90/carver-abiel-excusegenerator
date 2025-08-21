513a9992deed074143ac06a756f7a94155e18c57
Seeker90 <chris.carver90@icloud.com>
2025-08-21 00:11:15 +0000

    updated app file

--- src/app.js
+++ src/app.js
@@ -9,33 +9,30 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 
+  //fixed spacing issues when combining 
   let who = ['The dog ', 'My grandma ', 'The mailman ', 'My bird '];
-  let action = ['ate', 'peed', 'crushed', 'broke'];
-  let what = ['my homework', 'my phone', 'the car'];
+  let action = ['ate ', 'peed ', 'crushed ', 'broke '];
+  let what = ['my homework ', 'my phone ', 'the car '];
   let when = ['before class.', 'when i was sleeping.', 'while i was excercising.', 'during my lunch.'];
 
+//updated the length coding
+  let randomNumber1 = Math.floor(Math.random() * who.length);
+  let randomNumber2 = Math.floor(Math.random() * action.length);
+  let randomNumber3 = Math.floor(Math.random() * what.length);
+  let randomNumber4 = Math.floor(Math.random() * when.length);
 
-  let randomNumber1 = Math.floor(Math.random() * 4)
-  let randomNumber2 = Math.floor(Math.random() * 1)
-  let randomNumber3 = Math.floor(Math.random() * 3)
-  let randomNumber4 = Math.floor(Math.random() * 2)
-
-
-let theWho = who[2];
-let theAction = action[3];
-let theWhat = what[1];
-let theWhen = when[2];
-
-let excuse = theWho + theAction + theWhat + theWhen;
-
-console.log(excuse);
+//removed hard coded excuse
+  let excuse = who[randomNumber1] + action[randomNumber2] + what[randomNumber3] + when[randomNumber4];
+//removed console.log
+//console.log(excuse);
 
 
 let htmlElement = document.querySelector('#excuse');
+//removed console.log
+//console.log(htmlElement);
 
-console.log(htmlElement);
-
-enterHtml.innrHtml = excuse;
+//fixed link to index
+htmlElement.innerHTML = excuse;
 
 
 };
